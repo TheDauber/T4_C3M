b4c25b1e5307d7bd53cfccfd2c3dc8c29241e256
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2024-11-20 17:29:44 -0600

    Added Sounds, Skybox and Floating text

---
+++ T4_C3M/Assets/Scripts/TextManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    public GameObject welcomeText;
+    public GameObject welcomeText2;
+    public float delay = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= delay) 
+        {
+            welcomeText.SetActive(false);
+            welcomeText2.SetActive(true);
+        }
+        if (Time.time >= 2 * delay)
+        {
+            welcomeText.SetActive(false);
+            welcomeText2.SetActive(false);
+        }
+    }
+}
